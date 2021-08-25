c1386771b939d356a623fa373996e8b4ab026626
Szadziu <mcj.mikolajczak@gmail.com>
2021-08-25 08:28:30 +0200

    add few methods and start implementations

--- src/components/App.js
+++ src/components/App.js
@@ -4,9 +4,50 @@ import React from "react";
 import Slider from "./Slider";
 
 class App extends React.Component {
+  state = {
+    position: 0,
+  };
+
+  startDrag = (e) => {
+    e.preventDefault();
+    document.addEventListener("mousemove", this.drag);
+    document.addEventListener("mouseup", this.stopDrag);
+  };
+
+  stopDrag = () => {
+    document.removeEventListener("mousemove", this.drag);
+    document.removeEventListener("mouseup", this.stopDrag);
+  };
+
+  progressDrag = (width, mouseX) => {
+    const { max, min } = this.props;
+    const step = (width - 160) / (max - min);
+    const newValue = Math.round((mouseX - 80) / step) + min;
+    this.setState({ value: newValue });
+  };
+
+  drag = (e) => {
+    const slider = this.sliderRef.current;
+    const { left, width } = slider.getBouncingClientRect();
+    const draggableWidth = 160;
+    const mouseX = e.clientX - left;
+
+    const startPosition = mouseX - draggableWidth / 2;
+    if (mouseX - draggableWidth / 2 < 0) {
+      this.setState({ position: 0 });
+      return;
+    }
+    if (mouseX + draggableWidth / 2 > width) {
+      this.setState({ position: width - draggableWidth });
+      return;
+    }
+    this.progressDrag(width, mouseX);
+    this.setState({ position: startPosition });
+  };
+
   render() {
     return (
-      <Main className="App">
+      <Main ref={this.sliderRef} className="App">
         <Title>Okres</Title>
         <Slider />
         <Title>Kwota</Title>
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Slider = () => {
   return (
     <Axis>
-      <Point>{"<>"}</Point>
+      <Point>{"< >"}</Point>
     </Axis>
   );
 };
@@ -13,13 +13,21 @@ export default Slider;
 const Axis = styled.div`
   width: 200px;
   height: 10px;
+  margin: 0 auto;
   border: 1px solid black;
   border-radius: 5px;
 `;
 
 const Point = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: 50px;
   height: 50px;
   border-radius: 50%;
+  border: 3px solid white;
+  box-shadow: 0 0 2px 3px black;
   background-color: orange;
+  font-weight: bold;
+  transform: translate(-50%, calc(-50% + 5px));
 `;
